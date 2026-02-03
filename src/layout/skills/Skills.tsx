6a4b2ae48702189e39a3119f7e8b7d44b846cf96
DNK7008 <sorokodenis@mail.ru>
2026-02-03 22:50:13 +0300

    add text commit

--- src/layout/skills/Skills.tsx
+++ src/layout/skills/Skills.tsx
@@ -4,17 +4,68 @@ import { Icon } from "../../components/icon/Icon";
 
 export const Skills = () => {
   return (
-    <StyledSectionSkills>
-      <StyledSectionTitle></StyledSectionTitle>
-      <FlexWrapper>
-        <StyledSkillCard>
+    <StyledSkillsSection>
+      <StyledSkillsTitle>My Skills</StyledSkillsTitle>
+      <FlexWrapper flexWrap="wrap" justifyContent="center" alignItems="center">
+        <StyledCardSkill>
           <Icon iconId={"htmlSkill"} />
-        </StyledSkillCard>
+          <TitleCard>html5</TitleCard>
+          <AboutTextCard>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim
+          </AboutTextCard>
+        </StyledCardSkill>
+        <StyledCardSkill>
+          <Icon iconId={"htmlSkill"} />
+          <TitleCard>html5</TitleCard>
+          <AboutTextCard>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim
+          </AboutTextCard>
+        </StyledCardSkill>
+        <StyledCardSkill>
+          <Icon iconId={"htmlSkill"} />
+          <TitleCard>html5</TitleCard>
+          <AboutTextCard>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim
+          </AboutTextCard>
+        </StyledCardSkill>
+        <StyledCardSkill>
+          <Icon iconId={"htmlSkill"} />
+          <TitleCard>html5</TitleCard>
+          <AboutTextCard>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim
+          </AboutTextCard>
+        </StyledCardSkill>
+        <StyledCardSkill>
+          <Icon iconId={"htmlSkill"} />
+          <TitleCard>html5</TitleCard>
+          <AboutTextCard>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim
+          </AboutTextCard>
+        </StyledCardSkill>
+        <StyledCardSkill>
+          <Icon iconId={"htmlSkill"} />
+          <TitleCard>html5</TitleCard>
+          <AboutTextCard>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim
+          </AboutTextCard>
+        </StyledCardSkill>
       </FlexWrapper>
-    </StyledSectionSkills>
+    </StyledSkillsSection>
   );
 };
 
-const StyledSectionSkills = styled.section``;
-const StyledSectionTitle = styled.div``;
-const StyledSkillCard = styled.div``;
+const StyledSkillsSection = styled.section``;
+const StyledSkillsTitle = styled.div``;
+const StyledCardSkill = styled.div`
+  max-width: 380px;
+  min-height: 338px;
+`;
+const TitleCard = styled.h3``;
+const AboutTextCard = styled.p``;
+
